0c217253ddbe2fe9fd710292f3b6b681bac66185
Israel Curvelo <raellcurvelo@gmail.com>
2020-04-22 17:34:44 -0300

    Controlador Vendedores

---
+++ VendasMVCWeb/Controllers/VendedoresController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendasMVCWeb.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
