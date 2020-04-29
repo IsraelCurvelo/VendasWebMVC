d496d8431e8917e442d7bb641a5930d6893b0401
Israel Curvelo <raellcurvelo@gmail.com>
2020-04-29 15:39:52 -0300

    Preparando as views e navegações das pesquisas de vendas

---
+++ VendasMVCWeb/Controllers/RegistroDeVendasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendasMVCWeb.Controllers
+{
+    public class RegistroDeVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+    }
+}
