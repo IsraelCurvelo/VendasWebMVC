56bf575c3b86d4aa5bd1bc830191aebe97c828e6
Israel Curvelo <raellcurvelo@gmail.com>
2020-05-06 14:04:23 -0300

    Troca do tema do bootstrap e inicio da implementação cadastrar uma venda

--- VendasMVCWeb/Models/RegistroDeVendas.cs
+++ VendasMVCWeb/Models/RegistroDeVendas.cs
@@ -8,12 +8,23 @@ namespace VendasMVCWeb.Models
     public class RegistroDeVendas
     {
         public int Id { get; set; }
+
+
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data da Venda")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         public DateTime Data { get; set; }
 
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Display(Name = "Valor da Venda")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         public double ValorTotal { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public StatusDeVenda Status { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatório")]
         public Vendedor Vendedor { get; set; }
 
         public RegistroDeVendas()
